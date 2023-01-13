6623a8aeef7f11a440ce3b9e75d9af807c12edaf
irfanJMI <irfanali.k@hotmail.co.uk>
2023-01-14 01:08:45 +0530

    updated

---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  HOST: "dpg-cf0qgrcgqg45veu7f390-a.oregon-postgres.render.com",
+  USER: "irfan",
+  PASSWORD: "YvXjHt9oqjohJly4lWbC0eEQxoH2NRmJ",
+  DB: "estore",
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
