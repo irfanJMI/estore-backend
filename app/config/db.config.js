076e0dc612d51cfb166414ab8fa0c23c284f1d53
irfanJMI <irfanali.k@hotmail.co.uk>
2023-01-14 11:02:36 +0530

    updated code

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,18 +1,7 @@
 module.exports = {
-  // HOST: "dpg-cf0qgrcgqg45veu7f390-a",
-  // USER: "irfan",
-  // PASSWORD: "YvXjHt9oqjohJly4lWbC0eEQxoH2NRmJ",
-  // DB: "estore",
-  // dialect: "postgres",
-  // pool: {
-  //   max: 5,
-  //   min: 0,
-  //   acquire: 30000,
-  //   idle: 10000
-  // }
-   HOST: "localhost",
-  USER: "postgres",
-  PASSWORD: "system",
+  HOST: "dpg-cf0qgrcgqg45veu7f390-a",
+  USER: "irfan",
+  PASSWORD: "YvXjHt9oqjohJly4lWbC0eEQxoH2NRmJ",
   DB: "estore",
   dialect: "postgres",
   pool: {
@@ -21,4 +10,15 @@ module.exports = {
     acquire: 30000,
     idle: 10000
   }
+  //  HOST: "localhost",
+  // USER: "postgres",
+  // PASSWORD: "system",
+  // DB: "estore",
+  // dialect: "postgres",
+  // pool: {
+  //   max: 5,
+  //   min: 0,
+  //   acquire: 30000,
+  //   idle: 10000
+  // }
 };
